3b9202fc447d39105c0fa3e14853e6f2ca0c3fee
John Wood <johnnathanwood@gmail.com>
2018-08-03 14:06:17 -0500

    created product data in signinventory.js

---
+++ scripts/signinventory.js
@@ -0,0 +1,86 @@
+"use strict";
+
+const signs = [
+   {
+      "title": "Live Deliberately",
+      "description": "Custom Metal Sign, Henry David Thoreau Quote, Woods",
+      "image": "",
+      "quantity": "1",
+      "price": "49.00",
+      "dimensions": "18'x 24'",
+      "purchased": false,
+      "typeId": "1"
+   },
+   {
+      "title": "Rustic Hanging Farm Sign",
+      "description": "Personalized",
+      "image": "",
+      "quantity": "1",
+      "price": "49.00",
+      "dimensions": "18'x 18'",
+      "purchased": false,
+      "typeId": "1"
+   },
+   {
+      "title": "Compass Rose and Coordinates",
+      "description": "Personalized",
+      "image": "",
+      "quantity": "1",
+      "price": "49.00",
+      "dimensions": "18'x 18'",
+      "purchased": true,
+      "typeId": "1"
+   },
+   {
+      "title": "Garage Mahal Metal Sign",
+      "description": "Personalized",
+      "image": "",
+      "quantity": "1",
+      "price": "54.00",
+      "dimensions": "30'x 9.75'",
+      "purchased": true,
+      "typeId": "1"
+   },
+   {
+      "title": "Your Business Logo or Artwork",
+      "description": "Personalized",
+      "image": "",
+      "quantity": "1",
+      "price": "126.00",
+      "dimensions": "24'x 24'",
+      "purchased": true,
+      "typeId": "1"
+   },
+   {
+     "title": "French Country Bon Appetit Sign",
+     "description": "Stock Item",
+     "image": "",
+     "quantity": "1",
+     "price": "42.00",
+     "dimensions": "24'x 6.5'",
+     "purchased": true,
+     "typeId": "1"
+   },
+   {
+     "title": "Nashville Skyline",
+     "description": "Stock Item",
+     "image": "",
+     "quantity": "1",
+     "price": "59.00",
+     "dimensions": "24'x 9'",
+     "purchased": true,
+     "typeId": "1"
+   },
+   {
+     "title": "Nursery Sign",
+     "description": "Mountain Quote",
+     "image": "",
+     "quantity": "1",
+     "price": "59.00",
+     "dimensions": "30'x 21'",
+     "purchased": true,
+     "typeId": "1"
+   }
+];
+
+module.exports = signs;
